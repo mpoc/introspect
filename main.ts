b4d0c354d4a5843e76aca6e647393f2790df6d32
mpoc <marius.pocev@gmail.com>
2024-11-25 17:18:49 +0000

    Add command line questions

--- main.ts
+++ main.ts
@@ -1,3 +1,10 @@
-if (import.meta.main) {
+import { askJournal } from './generate.ts'
 
+if (import.meta.main) {
+    const question = Deno.args[0]
+    if (!question) {
+        console.log('No question provided')
+        Deno.exit(0)
+    }
+    console.log(await askJournal(question))
 }
